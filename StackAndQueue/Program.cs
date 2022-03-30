61ab71da2a30e73a09a175d8e8dd538e49a49241
MdWasimAkram <wasimkhanktr@gmail.com>
2022-03-31 00:21:44 +0530

    [Wasim] Add: UC4_Dequeue in Stack

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -3,17 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+<
 namespace StackAndQueue
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+             
             Queue queue = new Queue();
             queue.enqueue(56);
             queue.enqueue(30);
             queue.enqueue(70);
+            queue.display();
+            queue.dequeue();
             queue.display();
         }
     }
